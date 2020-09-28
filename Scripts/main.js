47bcb5607e42c17717be574532c894cf21f478f6
Julian-Ramirez1158 <69184233+Julian-Ramirez1158@users.noreply.github.com>
2020-09-28 16:04:34 -0400

    remove console.logs

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,19 +5,17 @@ $(function (){
         event.preventDefault();
         $('#output').empty();
         let userInput = $('#search').val();
-        console.log(userInput);
         let urlSent = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${userInput}&limit=12`;
-        console.log(urlSent);
         $.get( urlSent )
             .done( function( result ) {
                 for(let thing of result.data) {
-                    console.log(thing);
-                    let item = `<img class="col-sm-3" src="${thing.images.downsized_large.url}" alt="${thing.title}"/>`;
+                    let item = `<img class="col-sm-6 col-md-4" src="${thing.images.downsized_large.url}" alt="${thing.title}"/>`;
                     $('#output').append(item);
                 }
             } );
     });
 });
+
 
 
 
