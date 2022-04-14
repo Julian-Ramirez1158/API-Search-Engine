b50acea5ccb187b0742755a91120aa79b0e92d45
Julian-Ramirez1158 <69184233+Julian-Ramirez1158@users.noreply.github.com>
2022-04-13 20:03:21 -0400

    Increase # of returned giphys

--- Scripts/main.js
+++ Scripts/main.js
@@ -13,7 +13,7 @@ $(function (){
         event.preventDefault();
         $('#output').empty();
         let userInput = $('#search').val();
-        let urlSent = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${userInput}&limit=12`;
+        let urlSent = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${userInput}&limit=18`;
         $.get( urlSent )
             .done( function( result ) {
                 for(let thing of result.data) {
